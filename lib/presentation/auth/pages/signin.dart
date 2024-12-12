561915014c562259abf8823fa548f4a4a8db386b
mahery <mahery@futurmap.com>
2024-12-12 12:13:22 +0300

    start auth

--- lib/presentation/auth/pages/signin.dart
+++ lib/presentation/auth/pages/signin.dart
@@ -5,6 +5,27 @@ class SigninPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(
+          minimum: EdgeInsets.only(top: 100, right: 16, left: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [],
+          )),
+    );
+  }
+
+  Widget _signinText() {
+    return const Text(
+      'Sign in',
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+    );
+  }
+
+  Widget _emailField() {
+    return const TextField(
+      decoration: InputDecoration(hintText: 'Email'),
+    );
   }
 }
