2530efe9368ce2a5d1b4f8690122861d25f4d1e2
mahery <mahery@futurmap.com>
2024-12-12 12:04:49 +0300

    finish splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tmdb_clean/core/configs/theme/app_theme.dart';
+import 'package:tmdb_clean/presentation/splash/bloc/splash_cubit.dart';
 import 'package:tmdb_clean/presentation/splash/pages/splash.dart';
 
 void main() {
@@ -15,10 +17,12 @@ class MainApp extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
     ));
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const SplashPage(),
-      theme: AppTheme.appTheme,
-    );
+    return BlocProvider(
+        create: (context) => SplashCubit()..appStarted(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: const SplashPage(),
+          theme: AppTheme.appTheme,
+        ));
   }
 }
--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,27 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tmdb_clean/common/helper/navigation/app_navigation.dart';
 import 'package:tmdb_clean/core/configs/assets/app_images.dart';
+import 'package:tmdb_clean/presentation/auth/pages/signin.dart';
+import 'package:tmdb_clean/presentation/home/pages/home.dart';
+import 'package:tmdb_clean/presentation/splash/bloc/splash_cubit.dart';
+import 'package:tmdb_clean/presentation/splash/bloc/splash_state.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage(AppImages.splashBackground))),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-              const Color(0xff1A1B20).withOpacity(0),
-              const Color(0xff1A1B20)
-            ])),
-          )
-        ],
+    return Scaffold(
+      body: BlocListener<SplashCubit, SplashState>(
+        listener: (context, state) {
+          if (state is UnAuthenticated) {
+            AppNavigation.pushReplacement(context, SigninPage());
+          }
+          if (state is Authenticated) {
+            AppNavigation.pushReplacement(context, const HomePage());
+          }
+        },
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(AppImages.splashBackground))),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                const Color(0xff1A1B20).withOpacity(0),
+                const Color(0xff1A1B20)
+              ])),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.center,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      const Color(0xff1A1B20).withOpacity(0),
+                      const Color(0xff1A1B20),
+                    ]),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
