7c5a3d574babdb9fb4343d62be36739e55f9871d
mahery <mahery@futurmap.com>
2024-12-12 17:37:53 +0300

    start auth

--- lib/presentation/auth/pages/signin.dart
+++ lib/presentation/auth/pages/signin.dart
@@ -1,17 +1,40 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:reactive_button/reactive_button.dart';
+import 'package:tmdb_clean/common/helper/navigation/app_navigation.dart';
+import 'package:tmdb_clean/core/configs/theme/app_colors.dart';
+import 'package:tmdb_clean/presentation/auth/pages/signup.dart';
 
 class SigninPage extends StatelessWidget {
   const SigninPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-          minimum: EdgeInsets.only(top: 100, right: 16, left: 16),
+          minimum: const EdgeInsets.only(top: 100, right: 16, left: 16),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [],
+            children: [
+              _signinText(),
+              const SizedBox(
+                height: 30,
+              ),
+              _emailField(),
+              const SizedBox(
+                height: 30,
+              ),
+              _passwordField(),
+              const SizedBox(
+                height: 40,
+              ),
+              _signinButton(),
+              const SizedBox(
+                height: 30,
+              ),
+              _signupText(context)
+            ],
           )),
     );
   }
@@ -27,5 +50,34 @@ class SigninPage extends StatelessWidget {
     return const TextField(
       decoration: InputDecoration(hintText: 'Email'),
     );
+  }
+
+  Widget _passwordField() {
+    return const TextField(
+      decoration: InputDecoration(hintText: 'Password'),
+    );
+  }
+
+  Widget _signinButton() {
+    return ReactiveButton(
+      title: 'Sign in',
+      activeColor: AppColors.primary,
+      onPressed: () async {},
+      onSuccess: () {},
+      onFailure: (error) {},
+    );
+  }
+
+  Widget _signupText(BuildContext context) {
+    return Text.rich(TextSpan(children: [
+      TextSpan(text: 'Don\'t you have account?'),
+      TextSpan(
+          text: 'Sign up',
+          style: const TextStyle(color: Colors.blue),
+          recognizer: TapGestureRecognizer()
+            ..onTap = () {
+              AppNavigation.push(context, SignupPage());
+            })
+    ]));
   }
 }
