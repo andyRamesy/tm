d2ad255880e0fba86037d5a4905c1526fd5b0efe
andy ramesy <andyramesy@outlook.fr>
2024-12-16 23:55:21 +0300

    stop on implementing dio client

---
+++ lib/data/repositories/auth/auth.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:tmdb_clean/domain/auth/repositories/auth.dart';
+
+class AuthRepositoryImpl extends AuthRepository{
+  @override
+  Future<Either> signup() {
+    return;
+  }
+
+}
---
+++ lib/data/sources/auth/auth_api_service.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+
+abstract class AuthApiService {
+  Future<Either> signup();
+}
+
+class AuthApiServiceImpl extends AuthApiService{
+  @override
+  Future<Either> signup() {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+  
+}
