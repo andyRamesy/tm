870a0acf090ae4fbfeb8cb4537f2eba29e48869c
andy ramesy <andyramesy@outlook.fr>
2025-01-29 00:15:20 +0300

    fix movie repositories data type

--- lib/data/movie/repositories/movie.dart
+++ lib/data/movie/repositories/movie.dart
@@ -2,7 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:tmdb_clean/common/helper/mapper/movie_mapper.dart';
 import 'package:tmdb_clean/data/movie/models/movie.dart';
 import 'package:tmdb_clean/data/movie/sources/movie.dart';
-import 'package:tmdb_clean/domain/movie/entities/movie.dart';
 import 'package:tmdb_clean/domain/movie/repositories/movie.dart';
 import 'package:tmdb_clean/service_locator.dart';
 
@@ -14,7 +13,6 @@ class MovieRepositoryImpl extends MovieRepository {
     return returnedData.fold((error) {
       return Left(error);
     }, (data) {
-      print("data ${data}");
       var movies =
           List.from(data['data']).map((item) => MovieMapper.toEntity(MovieModel.fromJson(item))).toList();
       return Right(movies);
